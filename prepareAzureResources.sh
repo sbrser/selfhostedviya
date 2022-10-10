84f6c73d2882cbf8b9613f05cc76b91f102f4efd
sbrser <115498782+sbrser@users.noreply.github.com>
2022-10-10 16:30:26 -0300

    Update prepareAzureResources.sh

--- prepareAzureResources.sh
+++ prepareAzureResources.sh
@@ -28,6 +28,7 @@ az network nsg rule create \
     --resource-group ${az_project}-rg \
     --nsg-name ${az_project}-nsg \
     --name ${az_project}-nsg-ssh \
+	--source-address-prefixes ${az_public_access_cidrs}
     --protocol tcp \
     --priority 1000 \
     --destination-port-range 22 \
@@ -37,6 +38,7 @@ az network nsg rule create \
     --resource-group ${az_project}-rg \
     --nsg-name ${az_project}-nsg \
     --name ${az_project}-nsg-web \
+    --source-address-prefixes ${az_public_access_cidrs}
     --protocol tcp \
     --priority 1001 \
     --destination-port-range 6443 \
