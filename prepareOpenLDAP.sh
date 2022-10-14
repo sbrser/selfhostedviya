1ed218e52e45cdc3dd877ffe3806199119b2d77c
sbrser <115498782+sbrser@users.noreply.github.com>
2022-10-14 10:05:07 -0300

    Update prepareOpenLDAP.sh

--- prepareOpenLDAP.sh
+++ prepareOpenLDAP.sh
@@ -2,16 +2,14 @@
 
 # Configure OpenLDAP
 
+# Unpack files
 tar -xf selfhostedviya/ldap-basic.tar
 
-INGRESS_ALIAS_PHP="phpldapadmin.sbrserviya4single.eastus.cloudapp.azure.com"
-
-sed -i -e 's, _INGRESS_ALIAS_, '$INGRESS_ALIAS_PHP',g' ldap-basic/07-php-ldap-admin-ingress.yml
-
+# Create Namespace
 kubectl create ns ldap-basic
 
+# Apply Configurations
 kubectl apply -f ldap-basic/01-ldap-basic-namespace.yml
-
 kubectl apply -f ldap-basic/02-openldap-configmap.yml --namespace=ldap-basic
 kubectl apply -f ldap-basic/03-openldap-deployment.yml --namespace=ldap-basic
 kubectl apply -f ldap-basic/04-openldap-service.yml --namespace=ldap-basic
