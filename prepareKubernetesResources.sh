9bd4c837fcdb56a59f6c78a6c4d149462f7e92a2
sbrser <115498782+sbrser@users.noreply.github.com>
2022-10-17 10:27:23 -0300

    Update prepareKubernetesResources.sh

--- prepareKubernetesResources.sh
+++ prepareKubernetesResources.sh
@@ -37,7 +37,7 @@ sudo yum-config-manager \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo sed -i "s/containerd.sock/containerd.sock --exec-opt native.cgroupdriver=systemd/g" /usr/lib/systemd/system/docker.service
-sudo usermod -aG docker $USER
+sudo chown $USER /var/run/docker.sock
 sudo systemctl daemon-reload
 sudo systemctl start docker
 sudo systemctl enable docker
