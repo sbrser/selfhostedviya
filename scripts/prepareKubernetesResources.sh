9482e08d44541ef8429462b5290d40a154a49667
sbrser <115498782+sbrser@users.noreply.github.com>
2024-03-04 17:03:34 -0300

    Update prepareKubernetesResources.sh

--- scripts/prepareKubernetesResources.sh
+++ scripts/prepareKubernetesResources.sh
@@ -59,10 +59,11 @@ sudo systemctl restart docker
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.26/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.26/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
 # Set SELinux in permissive mode (effectively disabling it)
