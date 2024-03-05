dacd883aa37fa192085628c8a7c887ae5ce582dc
sbrser <115498782+sbrser@users.noreply.github.com>
2024-03-05 17:48:26 -0300

    Update prepareKubernetesResources.sh

--- scripts/prepareKubernetesResources.sh
+++ scripts/prepareKubernetesResources.sh
@@ -44,7 +44,7 @@ sudo sysctl --system
 # Installing container runtime
 
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install containerd
+sudo yum install containerd -y
 sudo sed -i 's/^"cri"$/""/' /etc/containerd/config.toml
 # sudo dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -y 
 sudo dnf install docker-ce --nobest -y
