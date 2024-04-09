815df14e2ffa12c55ca9ba5d919d6a325c8c218d
sbrser <115498782+sbrser@users.noreply.github.com>
2024-04-09 12:24:02 -0300

    Update prepareKubernetesResources.sh

--- scripts/prepareKubernetesResources.sh
+++ scripts/prepareKubernetesResources.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Modify repo
+sed -i 's|/AppStream/|-stream/AppStream/|g' /etc/yum.repos.d/CentOS-Linux-AppStream.repo
+
 # Define ingress_alias
 vm_location=`curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-08-01&format=text"`
 dns_prefix=`hostname | sed 's/-vm//'`
