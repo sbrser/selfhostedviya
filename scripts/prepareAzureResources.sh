3b41e5b9f4839b3d37115de84289290aa8ad2ab9
sbrser <115498782+sbrser@users.noreply.github.com>
2024-10-11 11:29:16 -0300

    Update prepareAzureResources.sh

--- scripts/prepareAzureResources.sh
+++ scripts/prepareAzureResources.sh
@@ -60,6 +60,9 @@ az vm create -n ${az_project}-vm -g ${az_project}-rg \
 --generate-ssh-keys \
 --size ${az_vm_size} \
 --nsg ${az_project}-nsg \
+--security-type TrustedLaunch \
+--enable-secure-boot false \
+--enable-vtpm false \
 --os-disk-size-gb ${az_vm_disk_size_gb} \
 --public-ip-sku Standard --no-wait
 
