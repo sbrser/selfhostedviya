92d2039349e3d49efb7cc38f0ce294393537fbd9
sbrser <115498782+sbrser@users.noreply.github.com>
2025-11-27 09:48:33 -0300

    Update prepareKubernetesResources.sh

--- scripts/prepareKubernetesResources.sh
+++ scripts/prepareKubernetesResources.sh
@@ -86,7 +86,7 @@ name=Kubernetes
 baseurl=https://pkgs.k8s.io/core:/stable:/v1.33/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.33/rpm/repodata/repomd.xml.key
 exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
